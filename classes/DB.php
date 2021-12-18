b7df6a0cbea4af92ed17da91fa4e21c78543a50f
MohamedGouaouri <im_gouaouri@esi.dz>
2021-12-18 10:43:41 +0100

    db config added

--- classes/DB.php
+++ classes/DB.php
@@ -1,17 +1,20 @@
 <?php
-
+require "mvc/config/db.php";
 
 class DB{
-    private static function connnect() {
-
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=howCode;charset=utf8', 'root', '');
+    private static $host = HOST;
+    private static $dbname = DBNAME;
+    private static $user = USERNAME;
+    private static $password = PASSWORD;
+    private static function connect() {
+        $pdo = new PDO("mysql:host=".self::$host.";dbname=".self::$dbname.";charset=utf8", self::$user, self::$password);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
 
     public static function query($query, $params = array()) {
-        $stmt = self::connnect()->prepare($query);
+        $stmt = self::connect()->prepare($query);
         $stmt->execute($params);
         return $stmt->fetchAll();
     }
---
+++ config/db.php
@@ -0,0 +1,5 @@
+<?php
+define('HOST', '127.0.0.1');
+define('DBNAME', 'tdw');
+define('USERNAME', 'root');
+define('PASSWORD', '');
